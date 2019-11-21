d1bf4a2b14bbd545df0b81b82af71210ab44180b
Ed Barwani <emb@uber.com>
2019-11-20 16:08:08 -0800

    Render cities

--- demo.js
+++ demo.js
@@ -3,6 +3,7 @@ const fit = require('canvas-fit')
 const { GUI } = require('dat.gui')
 const createCamera = require('3d-view-controls')
 const mat4 = require('gl-mat4')
+const { cities } = require('./data/cities.json')
 
 // Create a canvas
 const canvas = document.body.appendChild(document.createElement('canvas'))
@@ -11,7 +12,7 @@ window.addEventListener('resize', fit(canvas))
 
 // Create some settings.
 const settings = {
-  pointSize: 10,
+  pointSize: 3,
   pointColor: [64, 89, 114]
 }
 const gui = new GUI()
@@ -23,11 +24,20 @@ uniform float pointSize;
 uniform mat4 projection;
 uniform mat4 view;
 
-attribute vec3 position;
+attribute vec2 lnglat;
+
+vec3 getPosition(vec2 lnglat) {
+  vec2 rads = radians(lnglat);
+  return vec3(
+    cos(rads.y) * sin(rads.x),
+    sin(rads.y),
+    cos(rads.y) * cos(rads.x)
+  );
+}
 
 void main() {
   gl_PointSize = pointSize;
-  gl_Position = projection * view * vec4(position, 1);
+  gl_Position = projection * view * vec4(getPosition(lnglat), 1);
 }
 `
 const FRAGMENT_SHADER = `\
@@ -43,40 +53,41 @@ const animationLoop = new AnimationLoop({
   debug: true,
 
   onInitialize ({ gl, canvas }) {
-    const positions = []
-    let i = 1002
-    while (i--) {
-      positions.push(
-        Math.random() * 2 - 1,
-        Math.random() * 2 - 1,
-        Math.random() * 2 - 1
-      )
-    }
-
+    // Set clearing parameters for GL context
     setParameters(gl, {
       clearColor: [0.13, 0.13, 0.13, 1],
       clearDepth: [1],
       depthTest: true,
-      depthFunc: gl.LEQUAL
+      depthFunc: gl.LEQUAL,
+      blendFunc: [gl.SRC_ALPHA, gl.ONE],
+      blend: true
     })
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
+    // Set Camera
     const camera = createCamera(canvas)
     camera.lookAt([2, 2, 2], [0, 0, 0], [0, 1, 0])
     settings.view = camera.matrix
     settings.projection = mat4.perspective([], Math.PI / 4, canvas.width / canvas.height, 0.01, 100)
 
+    // Get data
+    const lnglats = cities.flatMap(c => c.lnglat)
+
+    // Create a drawing model
     const earth = new Model(gl, {
       vs: VERTEX_SHADER,
       fs: FRAGMENT_SHADER,
       uniforms: settings,
       attributes: {
-        position: new Buffer(gl, new Float32Array(positions))
+        lnglat: new Buffer(gl, new Float32Array(lnglats))
       },
       drawMode: gl.POINTS,
-      vertexCount: positions.length / 3
+      vertexCount: lnglats.length / 2
     })
+
+    // initial draw
     earth.draw()
+
     return { earth, camera }
   },
 
